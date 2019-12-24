ce5b0c33eba3285545126657aec3ff01b6419790
olopk <olekwojas@gmail.com>
2019-12-24 12:56:18 +0100

    added sidedrawer

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    console.log('no elo')
+    $(".nav-burger").click(()=>{
+        console.log('no elo')
+        $(".main-container").addClass("main-container--blur");
+        // $(".sidedrawer").animate({left: '500px'};
+    })
+})
