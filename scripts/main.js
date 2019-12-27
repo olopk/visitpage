9aac8d8de3586c9433b6a2dcd9a3ad75e357d92c
olopk <olekwojas@gmail.com>
2019-12-27 13:47:25 +0100

    added some logic for dynamic change of the main content.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,50 @@
 $(document).ready(function(){
-    console.log('no elo')
     $(".nav-burger").click(()=>{
-        console.log('no elo')
         $(".main-container").addClass("main-container--blur");
-        // $(".sidedrawer").animate({left: '500px'};
+        $(".sidedrawer").addClass("sidedrawer--visible");
     })
+    $(".sidedrawer").click(() => {
+        if($(".sidedrawer").hasClass("sidedrawer--visible")){
+            $(".sidedrawer").removeClass("sidedrawer--visible");
+            $(".main-container").removeClass("main-container--blur");
+        }
+    })
+    $(".maincontent").html(
+        `<p class="maincontent__name">Aleksander Wojas</p>
+        <div class="maincontent__logo">
+            <div class="maincontent__letter--a">
+                <img src="/images/A.png" alt="A">
+            </div>
+            <div class="maincontent__letter--w">
+                <img src="/images/W.png" alt="W">
+            </div>
+        </div>`
+    )
+    $(".maincontent__letter--a img").delay(1200).animate({'margin-right': '-33px'}, 1000)
+    
+    $(".sidedrawer__nav ul li a").click(()=>{
+        event.preventDefault();
+        const link = event.target.getAttribute('href').slice(1)
+        let content;
+
+        switch(link){
+            case 'home':
+                content = `<p class="maincontent__name">Aleksander Wojas</p>
+                <div class="maincontent__logo">
+                    <div class="maincontent__letter--a">
+                        <img src="/images/A.png" alt="A">
+                    </div>
+                    <div class="maincontent__letter--w">
+                        <img src="/images/W.png" alt="W">
+                    </div>
+                </div>`
+                break;
+            case 'about':
+                content = `<p class="maincontent__name">My name is Olek, im 31 years old.
+                Until now I've used to work as a sysadmin, but I didn't feel it challenging me anymore so I decided to discover the world of writing code. It appears to be pretty tough, very challenging and it looks like never ending journey. So I've decided to get on this train ;) </p>`
+                break;
+        }        
+        $(".maincontent").html(content).css("display", "none").fadeIn(1500);
+    })
+
 })
